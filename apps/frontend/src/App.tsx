faecbc9b35dcc970866a5700ee35d594d70fdb9d
jbuendia1y <jgamer669@gmail.com>
2021-09-27 14:25:30 -0500

    Resolved #12

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,26 +1,15 @@
 import "./App.scss";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Routes from "./components/Routes";
+import Routes from "./components/routes/Routes";
+import Sidenav from "./components/sidenav/sidenav";
 
 function App() {
   return (
     <>
       <Header />
       <div className="app-box">
-        {/* <div className="app__aside">
-          <div className="aside__user">
-            <img src="https://picsum.photos/536/354" alt="profile of User" />
-            <span>lorem</span>
-          </div>
-          <ul>
-            <li>lorem /////</li>
-          </ul>
-          <ul>
-            <li>lorem</li>
-            <li>lorem /////</li>
-          </ul>
-        </div> */}
+        <Sidenav />
         <Routes />
       </div>
       <Footer />
