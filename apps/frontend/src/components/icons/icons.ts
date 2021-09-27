12f7d42f650729ae15ce841d5b2d077e073ce8e7
jbuendia1y <jgamer669@gmail.com>
2021-09-27 14:27:27 -0500

    Agrupando iconos

---
+++ apps/frontend/src/components/icons/icons.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import MenuIcon from "./static/menu-icon.svg";
+import HomeIcon from "./static/home.svg";
+import ConfigurationIcon from "./static/settings-icon.svg";
+import LoginIcon from "./auth/login-icon.svg";
+import LogoutIcon from "./auth/logout-icon.svg";
+import AssessmentIcon from "./static/assessment.svg";
+import ListIcon from "./static/list.svg";
+
+import ReportIcon from "./reports/report.svg";
+import ReportBugIcon from "./reports/bug-report.svg";
+import HelpIcon from "./actions/help.svg";
+import InfoIcon from "./actions/info.svg";
+
+export {
+  MenuIcon,
+  HomeIcon,
+  ConfigurationIcon,
+  LoginIcon,
+  LogoutIcon,
+  AssessmentIcon,
+  ListIcon,
+  ReportIcon,
+  ReportBugIcon,
+  HelpIcon,
+  InfoIcon,
+};
